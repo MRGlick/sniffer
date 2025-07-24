4d72cd1594dd7d06fc3b1fdc9014cf4ccb934c92
MRGlick <mrg224003@gmail.com>
2025-07-24 22:42:04 +0300

    Added some packet parsing. can't test it bc laptop though.

--- main.c
+++ main.c
@@ -25,16 +25,94 @@ void dump_bytes(const u_char *bytes, int len, int row_length) {
     printf("\n");
 }
 
+typedef struct Userdata {
+    int link_type;
+} Userdata;
 
+typedef enum DatalinkType {
+    DL_LOOPBACK = DLT_NULL,
+    DL_ETHERNET = DLT_EN10MB,
+    DL_RAW = DLT_RAW,
+    DL_WIFI = DLT_IEEE802_11
+} DatalinkType;
+
+#define PACKED __attribute__((packed))
+
+typedef struct PACKED MacAddr {
+    u_char data[6];
+} MacAddr;
+
+
+#define ETH_TYPE_IPV4 0x0800
+#define ETH_TYPE_IPV6 0x86DD
+#define ETH_TYPE_ARP  0x0806
+
+
+typedef uint16_t EtherType;
+
+typedef struct PACKED EthPacket {
+    MacAddr dst_addr;
+    MacAddr src_addr;
+    EtherType eth_type;
+    u_char data[];
+} EthPacket;
+
+const char *get_dl_name(DatalinkType t) {
+    switch (t) {
+        case DL_LOOPBACK: return "loopback";
+        case DL_ETHERNET: return "ethernet";
+        case DL_RAW:      return "raw";
+        case DL_WIFI:     return "wifi";
+        default:          return "Invalid DatalinkType";
+    }
+}
+
+const char *get_eth_type_name(EtherType t) {
+    switch (t) {
+        case ETH_TYPE_IPV4: return "IPv4";
+        case ETH_TYPE_IPV6: return "IPv6";
+        case ETH_TYPE_ARP:  return "ARP";
+        default:            return "Invalid EtherType";
+    }
+} 
+
+void print_mac_addr(MacAddr addr) {
+    for (int i = 0; i < 6; i++) {
+        if (i > 0) printf(":");
+        printf("%02X", addr.data[i]);
+    }
+    printf("\n");
+}
+
+void analyze_and_print_packet(const struct pcap_pkthdr *packet_header, const u_char *bytes, const Userdata *data) {
+    printf("PACKET: \n\tlen: %d", packet_header->len);
+
+    DatalinkType link_type = data->link_type;
+
+    switch (link_type) {
+        case DL_ETHERNET:;
+            EthPacket *p = (EthPacket *)bytes;
+            printf("\tdst MAC address: ");
+            print_mac_addr(p->dst_addr);
+            printf("\tsrc MAC address: ");
+            print_mac_addr(p->src_addr);
+            printf("eth type: %s \n", get_eth_type_name(p->eth_type));
+            
+        break;
+        
+        default:
+            err_exit("Unsupported link type: %s \n", get_dl_name(link_type));
+        break;
+    }
+    
+
+}
 
 void on_packet_received(u_char *user_data, const struct pcap_pkthdr *packet_header, const u_char *bytes) {
-    assert(!user_data);
+    assert(user_data);
+    Userdata *data = (Userdata *)user_data;
 
-    printf("packet timestamp: %ds \n", packet_header->ts.tv_sec);
-    printf("packet length: %d \n", packet_header->len);
-    printf("packet cap: %d \n", packet_header->caplen);
-
-    dump_bytes(bytes, (int)packet_header->len, 8);
+    analyze_and_print_packet(packet_header, bytes, data);
 }
 
 void print_devices(void) {
@@ -56,11 +134,25 @@ void print_devices(void) {
 }
 
 #define ETH_DEV_NAME_BUF_SIZE 512
-int get_ethernet_device_name(char *buf) {
+
+
+typedef enum DeviceType {
+    DEV_ETH,
+    DEV_WIFI,
+    DEV_END
+} DeviceType;
+
+#define DEVICE_ETH 1
+#define DEVICE_WIFI 2
+int get_device_name(char *buf, DeviceType type) {
+
+    assert(type < DEV_END && type >= 0);
 
     bool found = false;
 
     char err[PCAP_ERRBUF_SIZE] = {0};
+
+    const char *keyword = NULL;
 
     pcap_if_t *interface_list;
 
@@ -70,7 +162,15 @@ int get_ethernet_device_name(char *buf) {
     );
 
     for (pcap_if_t *node = interface_list; node; node = node->next) {
-        if (strstr(node->description, "Ethernet")) {
+
+        // avert your eyes
+        keyword = type == DEV_ETH
+                            ? "Ethernet"
+                            : (type == DEV_WIFI
+                              ? "Wi-Fi"
+                              : "idk wtf" );
+
+        if (strstr(node->description, keyword)) {
             strcpy(buf, node->name);
             found = true;
             goto cleanup;
@@ -89,21 +189,28 @@ int main() {
 
     char ethernet_dev_name[ETH_DEV_NAME_BUF_SIZE] = {0};
 
-    if (get_ethernet_device_name(ethernet_dev_name) == PCAP_ERROR) err_exit(
+    if (get_device_name(ethernet_dev_name, DEV_WIFI) == PCAP_ERROR) err_exit(
         "Couldn't find an ethernet device!"
     );
 
     char err_buf[PCAP_ERRBUF_SIZE] = {0};
+
 
     pcap_t* handle = pcap_open_live(ethernet_dev_name, PACKET_MAX_SIZE, IS_PROMISCUOUS, TIMEOUT_MILI, err_buf);
     if (!handle) {
         err_exit("Couldn't open device: %s\n", err_buf);
     }
 
+    int link_type = pcap_datalink(handle);
+
+    Userdata user_data = {
+        .link_type = link_type
+    };
+
     pcap_loop(
         handle,
         -1,
         on_packet_received,
-        NULL
+        (u_char *)&user_data
     );
 }
